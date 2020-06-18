f2c7f78eaae5f26e36e44ab9bee95ab8a186daea
Neha Kennard <nnayak@cs.umass.edu>
2020-06-17 22:34:05 -0400

    clean up bash to avoid errors

--- corenlp.sh
+++ corenlp.sh
@@ -5,22 +5,15 @@
 #    ./corenlp.sh -file filename
 #    ./corenlp.sh -file filename -outputFormat text 
 
-#scriptdir=`dirname $0`
 scriptdir="./stanford-corenlp-full-2018-02-27/"
 java -version
 echo $PATH
 
-# echo java -mx3g -cp \"$scriptdir/*\" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
-
-# $1 - path
-
-java -mx2g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -ssplit.eolonly -tokenize.whitespace true -file data/example.txt
-
 DATAPATH=$1
 for FNAME in $DATAPATH/*
 do
-    java -version
-    java -mx2g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -ssplit.eolonly -tokenize.whitespace true -file $FNAME
-    # /usr/bin/java -mx2g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -file $FNAME
+    java -mx2g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP \
+      -annotators tokenize,ssplit,pos,lemma,ner,parse \
+      -tokenize.whitespace false -file $FNAME
     /bin/mv $(/usr/bin/basename $FNAME.xml) $DATAPATH/
 done
